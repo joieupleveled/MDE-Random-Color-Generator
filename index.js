23553316fe7726ec91159e18adc3c1ce264a5f0d
joieupleveled <joieupleveled@mailfence.com>
2022-09-06 19:05:03 +0200

    Update project

---
+++ index.js
@@ -0,0 +1,25 @@
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const color = randomColor({
+  luminosity: argv[3] ? argv[3] : 'random',
+  hue: argv[2] ? argv[2] : 'random',
+});
+
+// console.log(chalk.hex(`${color}`)(`${color}`));
+
+const theme = chalk.hex(color);
+
+const box = `####################################
+####################################
+####################################
+###########                #########
+###########    ${color}     #########
+###########                #########
+####################################
+####################################
+####################################
+`;
+
+console.log(theme(box));
